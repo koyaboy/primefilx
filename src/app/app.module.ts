dfa751d5efa13e8f06136417642f17b8fe7019f4
Koya <44787464+koyaboy@users.noreply.github.com>
2024-01-22 20:56:16 +0100

    Implement Header Design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SearchbarComponent } from './components/searchbar/searchbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchbarComponent
   ],
   imports: [
     BrowserModule,
