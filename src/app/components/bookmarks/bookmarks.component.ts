fc6849c873428536db1fb2abe5bf47fb05603513
Koya <44787464+koyaboy@users.noreply.github.com>
2024-01-24 13:41:28 +0100

    Implement Bookmarks Page Design

--- src/app/components/bookmarks/bookmarks.component.ts
+++ src/app/components/bookmarks/bookmarks.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { ShowsService } from '../../services/shows.service';
+import { Shows } from '../../models/shows';
 
 @Component({
   selector: 'app-bookmarks',
@@ -8,8 +9,16 @@ import { ShowsService } from '../../services/shows.service';
 })
 export class BookmarksComponent {
   showsService: ShowsService = inject(ShowsService)
+  bookmarkedShows: Shows[] = []
+  bookmarkedMovies: Shows[] = []
+  bookmarkedSeries: Shows[] = []
 
   ngOnInit() {
+    this.showsService.getShows().subscribe((shows) => {
+      this.bookmarkedShows = shows.filter((show) => show.isBookmarked == true)
+      this.bookmarkedMovies = shows.filter((show) => show.isBookmarked == true && show.category == "Movie")
+      this.bookmarkedSeries = shows.filter((show) => show.isBookmarked == true && show.category == "TV Series")
+    })
     this.showsService.setSearchCategory('bookmarked shows')
   }
 }
