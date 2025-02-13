d05ac1f5a8c488ea69fb7040d1beb3dd0d21e969
unknown <ire_adekoya@yahoo.com>
2025-02-13 08:22:21 +0100

    Add unit tests for HomeComponent

--- src/app/services/video.service.ts
+++ src/app/services/video.service.ts
@@ -1,33 +1,39 @@
 import { Injectable, signal } from '@angular/core';
 import { VideoPlayerComponent } from '../components/video-player/video-player.component';
 import { ComponentPortal } from '@angular/cdk/portal';
-import { Overlay } from "@angular/cdk/overlay"
+import { Overlay, OverlayRef } from '@angular/cdk/overlay';
 import { OverlayConfig } from '@angular/cdk/overlay';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class VideoService {
-  videoUrl: string = ""
-  videoTitle: string = ""
-  videoYear!: number
+  videoUrl: string = '';
+  videoTitle: string = '';
+  videoYear!: number;
+  overlayRef?: OverlayRef;
 
-  overlayRef = this.overlay.create(new OverlayConfig({
-    hasBackdrop: true
-  }));
+  // overlayRef = this.overlay.create(new OverlayConfig({
+  //   hasBackdrop: true
+  // }));
 
-  constructor(private overlay: Overlay) { }
+  constructor(private overlay: Overlay) {}
 
   playVideo(id: string, videoUrl: string, showTitle: string, showYear: number) {
-    this.videoUrl = videoUrl
-    this.videoTitle = showTitle
-    this.videoYear = showYear
+    this.videoUrl = videoUrl;
+    this.videoTitle = showTitle;
+    this.videoYear = showYear;
 
     const portal = new ComponentPortal(VideoPlayerComponent);
-    this.overlayRef.attach(portal)
+    this.overlayRef = this.overlay.create(
+      new OverlayConfig({
+        hasBackdrop: true,
+      })
+    );
+    this.overlayRef.attach(portal);
   }
 
   closeVideo(): void {
-    this.overlayRef?.detach()
+    this.overlayRef?.detach();
   }
 }
