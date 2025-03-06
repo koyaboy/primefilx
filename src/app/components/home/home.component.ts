1ec871b074247e90e38ca03edafeb84d5a0a1257
koyaboy <ire_adekoya@yahoo.com>
2025-03-06 12:51:34 +0100

    Defer rendering of below-the-fold component on Home page for better performance

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,52 +1,79 @@
-import { Component, Renderer2, Signal, inject, computed, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  Renderer2,
+  Signal,
+  inject,
+  computed,
+  ChangeDetectionStrategy,
+} from '@angular/core';
 import { ShowsService } from '../../services/shows.service';
 import { Shows } from '../../models/shows';
 import { Router } from '@angular/router';
 import { VideoService } from '../../services/video.service';
-import { toObservable } from '@angular/core/rxjs-interop'
+import { toObservable } from '@angular/core/rxjs-interop';
+import { ShowsListComponent } from '../shows-list/shows-list.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-
 export class HomeComponent {
-  showsService: ShowsService = inject(ShowsService)
+  showsListComponent: any;
+  showsService: ShowsService = inject(ShowsService);
 
-  shows: Signal<Shows[]> = this.showsService.shows
-  trendingShows: Signal<Shows[]> = computed(() => this.shows().filter(show => show.isTrending))
-  recommendedShows: Signal<Shows[]> = computed(() => this.shows().filter(show => !show.isTrending))
-  filteredShows!: Shows[]
+  shows: Signal<Shows[]> = this.showsService.shows;
+  trendingShows: Signal<Shows[]> = computed(() =>
+    this.shows().filter((show) => show.isTrending)
+  );
+  recommendedShows: Signal<Shows[]> = computed(() =>
+    this.shows().filter((show) => !show.isTrending)
+  );
+  filteredShows!: Shows[];
 
-  filterValue = this.showsService.filterValue
-  filterValue$ = toObservable(this.filterValue)
-  isLoading = this.showsService.isLoading
+  filterValue = this.showsService.filterValue;
+  filterValue$ = toObservable(this.filterValue);
+  isLoading = this.showsService.isLoading;
 
-  constructor(private videoService: VideoService) { }
+  constructor(private videoService: VideoService) {}
 
   ngOnInit() {
     this.filterValue$.subscribe((newValue) => {
-      this.filteredShows = this.shows().filter(show => show.title.includes(newValue))
-    })
+      this.filteredShows = this.shows().filter((show) =>
+        show.title.includes(newValue)
+      );
+    });
 
-    this.showsService.setSearchCategory('movies or TV series')
+    this.showsService.setSearchCategory('movies or TV series');
   }
 
   toggleBookmark(show: Shows) {
-    show.isBookmarked = !show.isBookmarked
+    show.isBookmarked = !show.isBookmarked;
 
-    this.showsService.updateBookmark(show._id)
+    this.showsService.updateBookmark(show._id);
   }
 
   playVideo(id: string, videoUrl: string, showTitle: string, showYear: number) {
-    this.videoService.playVideo(id, videoUrl, showTitle, showYear)
+    this.videoService.playVideo(id, videoUrl, showTitle, showYear);
   }
 
-  handleKeydown(event: KeyboardEvent, id: string, videoUrl: string, showTitle: string, showYear: number) {
-    if (event.key == "Enter") {
-      this.playVideo(id, videoUrl, showTitle, showYear)
+  handleKeydown(
+    event: KeyboardEvent,
+    id: string,
+    videoUrl: string,
+    showTitle: string,
+    showYear: number
+  ) {
+    if (event.key == 'Enter') {
+      this.playVideo(id, videoUrl, showTitle, showYear);
     }
+  }
+
+  async loadComponent() {
+    const { ShowsListComponent } = await import(
+      '../shows-list/shows-list.component'
+    ); // Lazy load the component
+    this.showsListComponent = ShowsListComponent;
   }
 }
